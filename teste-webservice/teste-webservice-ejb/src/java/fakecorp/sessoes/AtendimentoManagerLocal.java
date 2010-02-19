79fbb1cf97b0fb08af0390c5020b166fe70a3800
jaugustosaba <jaugustosaba@4aed5da6-17d5-11df-8596-193289140cd3>
2010-02-19 20:29:53 +0000

    criacao do ambiente

---
+++ teste-webservice/teste-webservice-ejb/src/java/fakecorp/sessoes/AtendimentoManagerLocal.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package fakecorp.sessoes;
+
+import javax.ejb.Local;
+
+/**
+ *
+ * @author jorge
+ */
+@Local
+public interface AtendimentoManagerLocal {
+    
+}
---
+++ teste-webservice/teste-webservice-ejb/src/java/fakecorp/sessoes/impl/AtendimentoBean.java
@@ -0,0 +1,37 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package fakecorp.sessoes.impl;
+
+import fakecorp.entidades.ChamadaAtendida;
+import java.util.Date;
+import javax.ejb.Stateless;
+import javax.jws.WebService;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author jorge
+ */
+@Stateless
+@WebService
+public class AtendimentoBean implements AtendimentoRemoteBean {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public int soma(int parametroA, int parametroB) {
+        ChamadaAtendida ca = new ChamadaAtendida();
+        ca.setDataAtendimento(new Date());
+        ca.setParametroA(parametroA);
+        ca.setParametroB(parametroB);
+
+        em.persist(ca);
+
+        return parametroA + parametroB;
+    }
+
+}
