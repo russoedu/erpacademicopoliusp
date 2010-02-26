42c16d10ad8b54453b7875edf9936cdfc2586c5e
helionagamachi <helionagamachi@4aed5da6-17d5-11df-8596-193289140cd3>
2010-02-26 19:04:01 +0000

    Para a criação do banco, executar o arquivo no mysql... tbm criem um usuário com nome erp e senha erp , o mesmo deve ter todos os privilégios no novo db criado....

---
+++ trunk/ERPPHP/protected/data/ERP.sql
@@ -0,0 +1,168 @@
+-- MySQL Administrator dump 1.4
+--
+-- ------------------------------------------------------
+-- Server version	5.1.44-community
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+
+--
+-- Create schema erp
+--
+
+CREATE DATABASE IF NOT EXISTS erp;
+USE erp;
+
+--
+-- Definition of table `aluno`
+--
+
+DROP TABLE IF EXISTS `aluno`;
+CREATE TABLE `aluno` (
+  `ID` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `firstName` varchar(50) NOT NULL,
+  `lastName` varchar(50) NOT NULL,
+  `email` varchar(70) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `aluno`
+--
+
+/*!40000 ALTER TABLE `aluno` DISABLE KEYS */;
+/*!40000 ALTER TABLE `aluno` ENABLE KEYS */;
+
+
+--
+-- Definition of table `biblioteca`
+--
+
+DROP TABLE IF EXISTS `biblioteca`;
+CREATE TABLE `biblioteca` (
+  `ID` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) NOT NULL,
+  `local` varchar(255) NOT NULL,
+  `bibliotecario` varchar(255) NOT NULL,
+  `outros` varchar(255) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `biblioteca`
+--
+
+/*!40000 ALTER TABLE `biblioteca` DISABLE KEYS */;
+/*!40000 ALTER TABLE `biblioteca` ENABLE KEYS */;
+
+
+--
+-- Definition of table `disciplina`
+--
+
+DROP TABLE IF EXISTS `disciplina`;
+CREATE TABLE `disciplina` (
+  `ID` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) NOT NULL,
+  `sigla` varchar(10) NOT NULL,
+  `ementa` text NOT NULL,
+  `professoresResponsaveis` varchar(255) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `disciplina`
+--
+
+/*!40000 ALTER TABLE `disciplina` DISABLE KEYS */;
+/*!40000 ALTER TABLE `disciplina` ENABLE KEYS */;
+
+
+--
+-- Definition of table `livro`
+--
+
+DROP TABLE IF EXISTS `livro`;
+CREATE TABLE `livro` (
+  `ID` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) NOT NULL,
+  `autor` varchar(255) NOT NULL,
+  `ISDN` bigint(20) unsigned NOT NULL,
+  `numClassfica` varchar(255) NOT NULL,
+  `editor` varchar(255) NOT NULL,
+  `ano` int(10) unsigned NOT NULL,
+  `local` varchar(255) NOT NULL,
+  `biblioteca_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `FK_biblioteca` (`biblioteca_id`),
+  CONSTRAINT `FK_biblioteca` FOREIGN KEY (`biblioteca_id`) REFERENCES `biblioteca` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `livro`
+--
+
+/*!40000 ALTER TABLE `livro` DISABLE KEYS */;
+/*!40000 ALTER TABLE `livro` ENABLE KEYS */;
+
+
+--
+-- Definition of table `turma`
+--
+
+DROP TABLE IF EXISTS `turma`;
+CREATE TABLE `turma` (
+  `ID` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) NOT NULL,
+  `disciplina_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `FK_disciplina` (`disciplina_id`) USING BTREE,
+  CONSTRAINT `FK_disciplina` FOREIGN KEY (`disciplina_id`) REFERENCES `disciplina` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `turma`
+--
+
+/*!40000 ALTER TABLE `turma` DISABLE KEYS */;
+/*!40000 ALTER TABLE `turma` ENABLE KEYS */;
+
+
+--
+-- Definition of procedure `TRUNCATEALL`
+--
+
+DROP PROCEDURE IF EXISTS `TRUNCATEALL`;
+
+DELIMITER $$
+
+/*!50003 SET @TEMP_SQL_MODE=@@SQL_MODE, SQL_MODE='STRICT_TRANS_TABLES,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `TRUNCATEALL`()
+BEGIN
+Truncate aluno;
+truncate biblioteca;
+truncate disciplina;
+truncate livro;
+truncate turma;
+END $$
+/*!50003 SET SESSION SQL_MODE=@TEMP_SQL_MODE */  $$
+
+DELIMITER ;
+
+
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
