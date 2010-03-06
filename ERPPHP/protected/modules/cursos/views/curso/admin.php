8e93c0cc4ec26639dda84d3a83f58aeb4ba205fd
jaugustosaba <jaugustosaba@4aed5da6-17d5-11df-8596-193289140cd3>
2010-03-06 00:23:57 +0000

    Esqueci de commitar o módulo de cursos... Agora vai funcionar...

---
+++ ERPPHP/protected/modules/cursos/views/curso/admin.php
@@ -0,0 +1,23 @@
+<?php
+$this->breadcrumbs=array(
+	'Cursos'=>array('index'),
+	'Manage',
+);
+?>
+<h1>Manage Cursos</h1>
+
+<ul class="actions">
+	<li><?php echo CHtml::link('List curso',array('index')); ?></li>
+	<li><?php echo CHtml::link('Create curso',array('create')); ?></li>
+</ul><!-- actions -->
+
+<?php $this->widget('zii.widgets.grid.CGridView', array(
+	'dataProvider'=>$dataProvider,
+	'columns'=>array(
+		'ID',
+		'nome',
+		array(
+			'class'=>'CButtonColumn',
+		),
+	),
+)); ?>
