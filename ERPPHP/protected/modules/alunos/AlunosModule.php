338f7fad1917c1f4c0611e7b8b7d5e461cd7fa97
helionagamachi <helionagamachi@4aed5da6-17d5-11df-8596-193289140cd3>
2010-03-14 12:46:11 +0000

    No momento da criação de um aluno, ele cria um usuário com username se senha igual à matrícula do aluno criado... atenção !! o aluno precisa ter um nome com nome e sobrenome separados por um espaço na hora da criação

--- ERPPHP/protected/modules/alunos/AlunosModule.php
+++ ERPPHP/protected/modules/alunos/AlunosModule.php
@@ -1,28 +1,35 @@
 <?php
 
-class AlunosModule extends CWebModule
-{
-	public function init()
-	{
-		// this method is called when the module is being created
-		// you may place code here to customize the module or the application
+class AlunosModule extends CWebModule {
+    public $hash='md5';
+    public function init() {
+        // this method is called when the module is being created
+        // you may place code here to customize the module or the application
 
-		// import the module-level models and components
-		$this->setImport(array(
-			'alunos.models.*',
-			'alunos.components.*',
-		));
-	}
+        // import the module-level models and components
+        $this->setImport(array(
+                'alunos.models.*',
+                'alunos.components.*',
+        ));
+    }
 
-	public function beforeControllerAction($controller, $action)
-	{
-		if(parent::beforeControllerAction($controller, $action))
-		{
-			// this method is called before any module controller action is performed
-			// you may place customized code here
-			return true;
-		}
-		else
-			return false;
-	}
+    public function beforeControllerAction($controller, $action) {
+        if(parent::beforeControllerAction($controller, $action)) {
+            // this method is called before any module controller action is performed
+            // you may place customized code here
+            return true;
+        }
+        else
+            return false;
+    }
+
+    public function encrypting($string="") {
+        $hash = Yii::app()->controller->module->hash;
+        if ($hash=="md5")
+            return md5($string);
+        if ($hash=="sha1")
+            return sha1($string);
+        else
+            return hash($hash,$string);
+    }
 }
