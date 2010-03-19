35597053c8363c7b8513e7b5bc678cd5334ca283
fmscpi <fmscpi@4aed5da6-17d5-11df-8596-193289140cd3>
2010-03-19 14:48:36 +0000

    Modulo Livro

--- ERPPHP/protected/modules/bibliotecas/views/livro/create.php
+++ ERPPHP/protected/modules/bibliotecas/views/livro/create.php
@@ -1,12 +1,15 @@
 <?php
 $this->breadcrumbs=array(
-	'Livros'=>array('index'),
-	'Novo',
+	'Bibliotecas'=>array('/bibliotecas/biblioteca/index'),
+    $biblioteca->nome=>array('/bibliotecas/biblioteca/view' , 'id'=>$biblioteca->id_biblioteca),
+	'Novo Livro',
 );
 ?>
 <h1>Novo Livro</h1>
 
 <ul class="actions">
+	<!-- <li><?php echo CHtml::link('List livro',array('index')); ?></li> -->
+	<!-- <li><?php echo CHtml::link('Manage livro',array('admin')); ?></li> -->
 </ul><!-- actions -->
 
 <?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
--- ERPPHP/protected/modules/bibliotecas/views/livro/view.php
+++ ERPPHP/protected/modules/bibliotecas/views/livro/view.php
@@ -1,6 +1,6 @@
 <?php
 $this->breadcrumbs=array(
-	'Biliotecas'=>array('/bibliotecas/biblioteca/index'),
+	'Bibliotecas'=>array('/bibliotecas/biblioteca/index'),
         $biblioteca->nome=>array('/bibliotecas/biblioteca/view' , 'id'=>$biblioteca->id_biblioteca),
 	$model->titulo,
 );
@@ -8,15 +8,22 @@ $this->breadcrumbs=array(
 <h1>Livro: <?php echo $model->titulo; ?></h1>
 
 <ul class="actions">
-	<li><?php echo CHtml::link('Editar Livro',array('update','id'=>$model->id_livro)); ?></li>
-        <li><?php echo CHtml::linkButton('Remover Livro',array('submit'=>array('delete','id'=>$model->id_livro),'confirm'=>'Deseja realmente remover este livro?')); ?></li>
-        <!--<li><?php echo CHtml::link('Novo Livro',array('create')); ?></li>-->
-        <li><?php if($status == 'Disponível')
+	
+		<li><?php echo CHtml::link('Editar Livro',array('update','id'=>$model->id_livro)); ?></li>
+        
+		<li><?php echo CHtml::linkButton('Remover Livro',array('submit'=>array('delete','id'=>$model->id_livro),'confirm'=>'Deseja realmente remover este livro?')); ?></li>
+        
+		<!--<li><?php echo CHtml::link('Novo Livro',array('create')); ?></li>-->
+        
+		<li><?php if($status == 'Disponível')
                     echo CHtml::link('Emprestar Livro',array('/bibliotecas/emprestimo/create','id_livro'=>$model->id_livro));
                   else
                     echo CHtml::link('Renovar Empréstimo',array('/bibliotecas/emprestimo/update','id_livro'=>$model->id_livro)); ?></li>
-        <li><?php echo CHtml::link('Listar Livros',array('index')); ?></li>
-	<li><?php echo CHtml::link('Gerenciar Livros',array('admin')); ?></li>
+        
+		<!--<li><?php echo CHtml::link('Listar Livros',array('index')); ?></li>-->
+	
+		<!--<li><?php echo CHtml::link('Gerenciar Livros',array('admin')); ?></li>-->
+
 </ul><!-- actions -->
 
 <?php $this->widget('zii.widgets.CDetailView', array(
