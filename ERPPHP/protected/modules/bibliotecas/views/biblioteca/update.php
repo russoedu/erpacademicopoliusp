f55959221409b7c0ee239ba62789eca3c60f6b92
helionagamachi <helionagamachi@4aed5da6-17d5-11df-8596-193289140cd3>
2010-03-19 11:44:17 +0000

    algumas coisas...

--- ERPPHP/protected/modules/bibliotecas/views/biblioteca/update.php
+++ ERPPHP/protected/modules/bibliotecas/views/biblioteca/update.php
@@ -1,18 +1,14 @@
 <?php
 $this->breadcrumbs=array(
 	'Bibliotecas'=>array('index'),
-	$model->id_biblioteca=>array('view','id'=>$model->id_biblioteca),
-	'Update',
+	$model->nome=>array('view','id'=>$model->id_biblioteca),
+	
 );
 ?>
 
-<h1>Update biblioteca <?php echo $model->id_biblioteca; ?></h1>
+<h1>Biblioteca <?php echo $model->nome; ?></h1>
 
 <ul class="actions">
-	<li><?php echo CHtml::link('List biblioteca',array('index')); ?></li>
-	<li><?php echo CHtml::link('Create biblioteca',array('create')); ?></li>
-	<li><?php echo CHtml::link('View biblioteca',array('view','id'=>$model->id_biblioteca)); ?></li>
-	<li><?php echo CHtml::link('Manage biblioteca',array('admin')); ?></li>
 </ul><!-- actions -->
 
 <?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
--- ERPPHP/protected/modules/bibliotecas/views/biblioteca/view.php
+++ ERPPHP/protected/modules/bibliotecas/views/biblioteca/view.php
@@ -4,22 +4,21 @@ $this->breadcrumbs=array(
 	$model->nome,
 );
 ?>
-<h1>View biblioteca #<?php echo $model->nome; ?></h1>
+<h1>Biblioteca <?php echo $model->nome; ?></h1>
 
 <ul class="actions">
-	<li><?php echo CHtml::link('List biblioteca',array('index')); ?></li>
+	<li><?php echo CHtml::link('Listagem de bibliotecas',array('index')); ?></li>
         <li><?php echo CHtml::link('Adicionar livro', array('/bibliotecas/livro/create','bib'=>$model->id_biblioteca));?></li>
         <li><?php echo CHtml::link('Empréstimos', array('/bibliotecas/emprestimo/index','bib'=>$model->id_biblioteca));?></li>
-	<li><?php echo CHtml::link('Create biblioteca',array('create')); ?></li>
-	<li><?php echo CHtml::link('Update biblioteca',array('update','id'=>$model->id_biblioteca)); ?></li>
-	<li><?php echo CHtml::linkButton('Delete biblioteca',array('submit'=>array('delete','id'=>$model->id_biblioteca),'confirm'=>'Are you sure to delete this item?')); ?></li>
-	<li><?php echo CHtml::link('Manage biblioteca',array('admin')); ?></li>
+	
+	<li><?php echo CHtml::link('Atualizar biblioteca',array('update','id'=>$model->id_biblioteca)); ?></li>
+	<li><?php echo CHtml::linkButton('Deletar biblioteca',array('submit'=>array('delete','id'=>$model->id_biblioteca),'confirm'=>'Tem certeza?')); ?></li>
+	
 </ul><!-- actions -->
 
 <?php $this->widget('zii.widgets.CDetailView', array(
 	'data'=>$model,
 	'attributes'=>array(
-		#'id_biblioteca',
 		'nome',
 		'local',
 	),
