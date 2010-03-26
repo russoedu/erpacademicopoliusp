e6cc701d5420ed60619e85658ae8095ef91f1293
helionagamachi <helionagamachi@4aed5da6-17d5-11df-8596-193289140cd3>
2010-03-26 13:24:08 +0000

    + um pouco de controle de usuários....

--- ERPPHP/protected/modules/bibliotecas/views/biblioteca/index.php
+++ ERPPHP/protected/modules/bibliotecas/views/biblioteca/index.php
@@ -1,21 +1,22 @@
 <?php
 $this->breadcrumbs=array(
-	'Bibliotecas',
+        'Bibliotecas',
 );
 ?>
 
 <h1>Listagem de bibliotecas</h1>
 
 <ul class="actions">
-	<li><?php
-        if(Yii::app()->getModule('user')->isAdmin()){
-        echo CHtml::link('Criar biblioteca',array('create'));
-        }
-        ?></li>
-        
-</ul><!-- actions -->
+    <?php
+    foreach ($actions as $action) {
+        echo "<li>";
+        echo $action;
+        echo "<li/>";
+    }
+    ?>
+</ul>
 
 <?php $this->widget('zii.widgets.CListView', array(
-	'dataProvider'=>$dataProvider,
-	'itemView'=>'_view',
+        'dataProvider'=>$dataProvider,
+        'itemView'=>'_view',
 )); ?>
--- ERPPHP/protected/modules/bibliotecas/views/livro/view.php
+++ ERPPHP/protected/modules/bibliotecas/views/livro/view.php
@@ -9,20 +9,14 @@ $this->breadcrumbs=array(
 
 <ul class="actions">
 	
-		<li><?php echo CHtml::link('Editar Livro',array('update','id'=>$model->id_livro)); ?></li>
-        
-		<li><?php echo CHtml::linkButton('Remover Livro',array('submit'=>array('delete','id'=>$model->id_livro),'confirm'=>'Deseja realmente remover este livro?')); ?></li>
-        
-		<!--<li><?php echo CHtml::link('Novo Livro',array('create')); ?></li>-->
-        
-		<li><?php if($status == 'Disponível')
-                    echo CHtml::link('Emprestar Livro',array('/bibliotecas/emprestimo/create','id_livro'=>$model->id_livro));
-                  else
-                    echo CHtml::link('Renovar Empréstimo',array('/bibliotecas/emprestimo/update','id_livro'=>$model->id_livro)); ?></li>
-        
-		<!--<li><?php echo CHtml::link('Listar Livros',array('index')); ?></li>-->
-	
-		<!--<li><?php echo CHtml::link('Gerenciar Livros',array('admin')); ?></li>-->
+    <?php
+    foreach ($actions as $action) {
+        echo "<li>";
+        echo $action;
+        echo "<li/>";
+    }
+    ?>
+		
 
 </ul><!-- actions -->
 
