adf3c3402aeb634ce402301d2f3632400993af98
fmscpi <fmscpi@4aed5da6-17d5-11df-8596-193289140cd3>
2010-04-14 20:22:30 +0000

    Tradução da página do oferecimento.

--- trunk/ERPPHP/protected/modules/cursos/views/oferecimento/view.php
+++ trunk/ERPPHP/protected/modules/cursos/views/oferecimento/view.php
@@ -7,11 +7,11 @@ $this->breadcrumbs=array(
 <h1>Oferecimento <?php echo $model->id_oferecimento; ?></h1>
 
 <ul class="actions">
-	<li><?php echo CHtml::link('List oferecimento',array('index')); ?></li>
-	<li><?php echo CHtml::link('Create oferecimento',array('create')); ?></li>
-	<li><?php echo CHtml::link('Update oferecimento',array('update','id'=>$model->id_oferecimento)); ?></li>
-	<li><?php echo CHtml::linkButton('Delete oferecimento',array('submit'=>array('delete','id'=>$model->id_oferecimento),'confirm'=>'Are you sure to delete this item?')); ?></li>
-	<li><?php echo CHtml::link('Manage oferecimento',array('admin')); ?></li>
+	<li><?php echo CHtml::link('Listar oferecimentos',array('index')); ?></li>
+	<li><?php echo CHtml::link('Criar oferecimento',array('create')); ?></li>
+	<li><?php echo CHtml::link('Atualizar oferecimento',array('update','id'=>$model->id_oferecimento)); ?></li>
+	<li><?php echo CHtml::linkButton('Deletar oferecimento',array('submit'=>array('delete','id'=>$model->id_oferecimento),'confirm'=>'Você tem certeza que deseja deletar?')); ?></li>
+	<li><?php echo CHtml::link('Gerenciar oferecimentos',array('admin')); ?></li>
 </ul><!-- actions -->
 
 <?php $this->widget('zii.widgets.CDetailView', array(
