e42b2c93f3671372b179d7b63a06e2338db5cf82
fmscpi <fmscpi@4aed5da6-17d5-11df-8596-193289140cd3>
2010-04-14 19:40:36 +0000

    Correção de idioma

--- trunk/ERPPHP/protected/modules/cursos/views/curso/update.php
+++ trunk/ERPPHP/protected/modules/cursos/views/curso/update.php
@@ -9,7 +9,7 @@ $this->breadcrumbs=array(
 <h1>Update curso <?php echo $model->id_curso; ?></h1>
 
 <ul class="actions">
-	<li><?php echo CHtml::link('List curso',array('index')); ?></li>
+	<li><?php echo CHtml::link('Lista de cursos',array('index')); ?></li>
 	<li><?php echo CHtml::link('Create curso',array('create')); ?></li>
 	<li><?php echo CHtml::link('View curso',array('view','id'=>$model->id_curso)); ?></li>
 	<li><?php echo CHtml::link('Manage curso',array('admin')); ?></li>
