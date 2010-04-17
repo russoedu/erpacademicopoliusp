6942bc3971019ea73b58827d95018658a24845ae
jaugustosaba <jaugustosaba@4aed5da6-17d5-11df-8596-193289140cd3>
2010-04-17 00:59:43 +0000

    Comunicação com o RH...

--- ERPPHP/protected/modules/user/controllers/AdminController.php
+++ ERPPHP/protected/modules/user/controllers/AdminController.php
@@ -43,12 +43,184 @@ class AdminController extends Controller
 			),
 		));
 
+		/*
+ 		 * Acesso a webservice de RH
+		 */
+                $this->syncWithHumanResources();
+
 		$this->render('index',array(
 			'dataProvider'=>$dataProvider,
 		));
 	}
 
+	/*
+	 * Acesso ao webservice de RH
+	 */
+	private function getEmployersJSONList($roleName) {
+                $ws_url = "http://rhlabsoft.heroku.com/employees.json?by_role=" . urlencode($roleName);
+		//$ws_url = "http://localhost/fake-php-ws/index.php?by_role=" . (urlencode($roleName));
+		$ws_json_string = file_get_contents($ws_url);
 
+                $result = json_decode($ws_json_string);
+                if (is_null($result)) {
+                    $result = array();
+                }
+
+		return $result;
+	}
+
+        private function syncWithHumanResources() {
+                // bibliotecários
+                $all_bibliotecarios = bibliotecario::model()->findAll();
+		$bibliotecarios_hash_list = $this->getEmployersJSONList('Bibliotecário');
+                assert(is_array($all_bibliotecarios));
+                $this->syncDb($all_bibliotecarios, $bibliotecarios_hash_list, 'b');
+
+                // Professores                
+                $all_professores = professor::model()->findAll();
+                $professores_hash_list = $this->getEmployersJSONList('Professor');
+                assert(is_array($all_professores));
+                $this->syncDb($all_professores, $professores_hash_list, 'p');
+
+                // Gestores Acadêmicos
+                $all_gestores = gestoracademico::model()->findAll();
+                $gestores_hash_list = $this->getEmployersJSONList('Gestores Acadêmico');
+                assert(is_array($gestores_hash_list));
+                $this->syncDb($all_gestores, $gestores_hash_list, 'g');
+        }
+
+	private function syncDb($registers, $employeers_hash, $employeersType) {
+
+                // para cada bibliotecario existente, verifica se ele ainda
+                // existe no RH, se não existir no RH remove do banco
+                // @POG: Sincronia muito suceptível a falhas
+                while ( count($registers) > 0 ) {
+                    $current_register = array_shift($registers);
+                    $found = false;
+                    for ($i=0;$i<count($employeers_hash); $i++) {
+                        $id = null;
+                        switch ($employeersType) {
+                            case 'b':
+                                $id = $current_register->id_bibliotecario;
+                                break;
+                            case 'p':
+                                $id = $current_register->id_professor;
+                                break;
+                            case 'g':
+                                $id = $current_register->id_gestor_academico;
+                                break;
+                        }
+                        assert($id != null);
+                        // verifica se o funcionário ainda existe no rh. Se existir: atualiza seus atributos
+                        if ($id == $employeers_hash[$i]->employee->id) {
+                            $current_register->nome = $employeers_hash[$i]->employee->name;
+                            // @TODO: atualizar o resto dos atributos do funcionário
+                            $current_register->save();
+                            unset($employeers_hash[$i]); // esse lista só deve conter novos funcionários
+                            $found = true;
+                            break;
+                        }
+                    }
+                    // não está mais presente no módulo de RH, logo não deve
+                    // estar presente no módulo acadêmico
+                    if (!$found) {
+                        $rf = RegistrationForm::model()->find("id=" . $current_register->tbl_users_id);
+                        $profile = Profile::model()->findByPk($rf->id);
+                        
+                        $current_register->delete();
+                        $profile->delete();                        
+                        $rf->delete();
+                    }
+                }
+                // $employeers_hash só tem os novos funcionários que devem
+                // ser inseridos no banco
+                foreach($employeers_hash as $new_employeer) {
+                    $this->addNewEmployeer($new_employeer->employee->name,
+                            $new_employeer->employee->id, $employeersType);
+                }
+
+	}
+
+        /**
+         * Gera nome de usuario, primeiro nome e último nome a partir de um nome
+         *
+         * @param <type> $name
+         * @return <type>
+         */
+        private function generateNewUserInfo($name) {
+            $splitted_name = split(" ", $name);
+
+            $username = strtolower($splitted_name[0] . $splitted_name[count($splitted_name) - 1]);
+            $username = ereg_replace("[áàâãª]","a",$username);
+            $username = ereg_replace("[éèê]","e",$username);
+            $username = ereg_replace("[óòôõº]","o",$username);
+            $username = ereg_replace("[úùû]","u",$username);
+            $username = str_replace("ç","c",$username);
+            return array(
+                // POG: devia ser checado se já há alguém
+                'username' => $username,
+                'firstname' => $splitted_name[0],
+                'lastname' => $splitted_name[1],
+            );
+        }
+
+        /**
+         * Adiciona um novo funcionário
+         * 
+         * @param <type> $name
+         * @param <type> $id
+         * @param <type> $employeerType
+         */
+        private function addNewEmployeer($name, $id, $employeerType) {
+                $registrationForm = new RegistrationForm;
+                $profile = new Profile;
+
+                // gera informações para um novo usuário
+                $userInfo = $this->generateNewUserInfo($name);
+                $password = $userInfo['username'];
+                $registrationForm->password=UserModule::encrypting($password);
+                $registrationForm->verifyPassword=UserModule::encrypting($password);
+                $registrationForm->activkey=UserModule::encrypting(microtime().$password);
+                $registrationForm->createtime=time();
+                $registrationForm->lastvisit=((Yii::app()->controller->module->autoLogin&&Yii::app()->controller->module->loginNotActiv)?time():0);
+                $registrationForm->superuser=0;
+                $registrationForm->status=1;
+                $registrationForm->email = $userInfo['username'] . "@poli.usp.br"; // @POG: Isso é um pog, não temos info do e-mail
+                $registrationForm->username = $userInfo['username'];
+
+                if($registrationForm->validate()) {
+                    $registrationForm->save();
+                    $profile->firstname = $userInfo['firstname'];
+                    $profile->lastname = $userInfo['lastname'];
+                    $profile->user_id=$registrationForm->id;
+                    $profile->save();
+
+                    switch ($employeerType) {
+                        case 'b':
+                            $novoBibliotecario = new bibliotecario;
+                            $novoBibliotecario->nome = $name;
+                            $novoBibliotecario->id_bibliotecario = $id;
+                            $novoBibliotecario->tbl_users_id = $registrationForm->id;
+                            $novoBibliotecario->save();
+                            break;
+                        case 'p':
+                            $novoProfessor = new professor;
+                            $novoProfessor->nome = $name;
+                            $novoProfessor->id_professor = $id;
+                            $novoProfessor->tbl_users_id = $registrationForm->id;
+                            $novoProfessor->save();
+                            break;
+                        case 'g':
+                            $novoGestor = new gestoracademico;
+                            $novoGestor->nome = $name;
+                            $novoGestor->id_gestor_academico = $id;
+                            $novoGestor->tbl_users_id = $registrationForm->id;
+                            $novoGestor->save();
+                            break;
+                    }
+                }
+        }
+        
 	/**
 	 * Displays a particular model.
 	 */
